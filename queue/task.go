e1e62e1a3723b8cb14fc227ac9efe76d9d178e3a
GuoHaoLan <coopticks@hotmail.com>
2023-06-23 23:09:32 +0800

    fix task

--- queue/task.go
+++ queue/task.go
@@ -43,22 +43,21 @@ type TaskEntry struct {
 
 func DefaultRetry() RetryFunc {
 	return func(tf *TaskEntry) error {
+		// when multi channels are available,
+		// the go runtime will select it randomly.
+		// however, we must ensure the stop signal is first,
+		// otherwise, this goroutine will be paused by the ticker channel.
+		select {
+		case <-tf.stop.Done():
+			return ErrTaskStopped
+		default:
+		}
+
 		sleep := time.NewTicker(time.Second)
 		defer sleep.Stop()
 
 		for i := 0; i < tf.retryLimit; i++ {
-
-			// when multi channels are available,
-			// the go runtime will select it randomly.
-			// however, we must ensure the stop signal is first,
-			// otherwise, this goroutine will be paused by the ticker channel.
-			select {
-			case <-tf.stop.Done():
-				return ErrTaskStopped
-			default:
-			}
-
-			// may be paused
+			// may be woken up by stop signal or the ticker.
 			select {
 			case <-tf.stop.Done():
 				return ErrTaskStopped
