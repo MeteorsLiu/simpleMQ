ad6333dce5ca62648528c96218d709df1881170f
GuoHaoLan <coopticks@hotmail.com>
2023-06-26 16:50:46 +0800

    Add Tgkill test

--- queue/kill_test.go
+++ queue/kill_test.go
@@ -9,17 +9,24 @@ import (
 	"time"
 )
 
+type PID struct {
+	pid int
+	tid int
+}
+
 func TestKill(t *testing.T) {
 	var wg sync.WaitGroup
-	tgid := make(chan int)
-	pid := syscall.Getpid()
+	tgid := make(chan *PID)
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		tgid <- syscall.Gettid()
+		tgid <- &PID{
+			pid: syscall.Getpid(),
+			tid: syscall.Gettid(),
+		}
 
 		sig := make(chan os.Signal)
-		signal.Notify(sig, syscall.SIGSTOP)
+		signal.Notify(sig, syscall.SIGINT)
 		ticker := time.NewTicker(time.Second)
 		i := 0
 		for {
@@ -28,13 +35,14 @@ func TestKill(t *testing.T) {
 				i++
 				t.Log(i)
 			case <-sig:
+				t.Log("B exits")
 				return
 			}
 		}
 	}()
 	id := <-tgid
 	time.AfterFunc(10*time.Second, func() {
-		syscall.Tgkill(pid, id, syscall.SIGSTOP)
+		syscall.Tgkill(id.pid, id.tid, syscall.SIGINT)
 	})
 	wg.Wait()
 	t.Log("exit")
